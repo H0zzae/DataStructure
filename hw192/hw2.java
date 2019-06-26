9d6ea46e6db1595f820db22ffbbb674ffd4b2f1b
H0zzae <39540561+H0zzae@users.noreply.github.com>
2019-06-26 18:41:46 +0900

    HW02
    Sparse Matrix with Linked List 의 AddOneElement, Delete, Add 함수 구현

---
+++ hw192/hw2.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+// offset
+
+/**
+ * SparseMatrix Class
+ * 
+ */
+
+
+// ---------- SparseMatrix class -------------
+class SparseMatrix extends LinkedMatrix {
+
+	SparseMatrix() { } // SparseMatrix constructor. 
+
+
+	boolean AddOneElement(final int r, final int c, final int  v) {
+
+
+
+
+		return true;
+	}
+
+	boolean Delete(final int r, final int c) {
+
+
+
+
+
+		return true;
+	}
+
+	SparseMatrix Add(SparseMatrix b) {
+		SparseMatrix c = new SparseMatrix();
+		c.Init(rows, cols);
+
+
+
+
+
+		return c;
+	}
+}
+
+
